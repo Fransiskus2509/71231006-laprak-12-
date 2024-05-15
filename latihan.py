ec9052074f34b4947622861b556928c8ca7725a9
Fransiskus2509 <143708148+Fransiskus2509@users.noreply.github.com>
2024-05-15 14:20:00 +0700

    Add files via upload

---
+++ latihan.py
@@ -0,0 +1,19 @@
+# List menjadi Set
+list_data = [1, 2, 3, 4, 5]
+set_data = set(list_data)
+print("List menjadi Set:", set_data)
+
+# Set menjadi List
+set_data = {1, 2, 3, 4, 5}
+list_data = list(set_data)
+print("Set menjadi List:", list_data)
+
+# Tuple menjadi Set
+tuple_data = (1, 2, 3, 4, 5)
+set_data = set(tuple_data)
+print("Tuple menjadi Set:", set_data)
+
+# Set menjadi Tuple
+set_data = {1, 2, 3, 4, 5}
+tuple_data = tuple(set_data)
+print("Set menjadi Tuple:", tuple_data)
